c6a67e354998b8a0cc5f2bc4171bd472e1cc5b3b
dg222dm <dg222dm@student.lnu.se>
2014-11-10 10:51:37 +0000

    solved minAge & maxAge

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,9 +5,9 @@ var makePerson = function(persArr){
 var minAge;
 var maxAge;
 var averageAge;
+var averageAgeSum;
 var names = "";
 var ageArray = [];
-
 
 for(var i = 0; i < persArr.length; i += 1){
 	
@@ -15,15 +15,18 @@ for(var i = 0; i < persArr.length; i += 1){
 	
 }
 
-console.log(ageArray);
 
 minAge = Math.min.apply(null, ageArray); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
 maxAge = Math.max.apply(null, ageArray);
-averageAge = 
 
+averageAgeSum = ageArray.reduce(function(a, b){return a + b});  //http://codereview.stackexchange.com/questions/14106/how-do-i-make-this-average-function-better-more-elegant
+averageAge = Math.round(averageAgeSum / ageArray.length);		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
+console.log(ageArray);
 console.log(minAge);
 console.log(maxAge);
-
+console.log(averageAgeSum);
+console.log(averageAge);
 /*
 	nameArray.sort(function (a,b){return a.localeCompare(b, 'sv');});
 	function (a,b){return a.localeCompare(b, 'sv');}
