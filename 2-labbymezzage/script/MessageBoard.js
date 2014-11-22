e753f59139b7af48392ca6d3a9cf96371fec046f
dg222dm <dg222dm@student.lnu.se>
2014-11-22 13:42:56 +0000

    added some lame looking  html and stylesheet

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -2,13 +2,20 @@
 
 var MessageBoard = {
     
+    messages: [],
+    
     init:function(){
         
-        var mess = new Message("Testmeddelande", new Date());
-        alert(mess); //Använder toString för utskrift
-        alert(mess.getText()); // Skriver enbart ut texten.
-        mess.setText("En annan text");
-        alert(mess); //skriver ut meddelandet med ändrad text
+        
+        // alert(mess); //Använder toString för utskrift
+        // alert(mess.getText()); // Skriver enbart ut texten.
+        // mess.setText("En annan text");
+        // alert(mess); //skriver ut meddelandet med ändrad text
+        
+        
+        
+        MessageBoard.messages.push(new Date());
+           
         
         
     }
