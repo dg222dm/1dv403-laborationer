9e7a9b9a35b674b03d7a5b56231c234a6f8d7c54
dg222dm <dg222dm@student.lnu.se>
2014-11-26 16:33:59 +0000

    hopefully done with table

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -3,24 +3,29 @@
 var Memory = {
     
     memoryArray: [], //Skapa en egenskap på ditt objekt som senare kommer att referera till den utslumpade arrayen
-    rows:2,
-    cols: 2,
+    rows:4,
+    cols: 4,
     count: 0,
     
     init:function(){
+        
+        //Skapa ett objekt
         Memory.memoryArray = RandomGenerator.getPictureArray(Memory.rows,Memory.cols); //I init-metoden anropar du arrayslumpsmetoden och sparar resultatet i egenskapen du skapade i 4an
-        //skapa array objekt
         //console.log(Memory.memoryArray);
+        
+        //Skapa en table
         var table = document.createElement("table");
         table.border = 2;
  
+        //ta in div taggen 
         var memory_board = document.getElementById("memory_board");
        
+        //skriv ut tablen
         for (var i = 0; i < Memory.rows; i += 1)
         {
             var row = document.createElement("tr");
             table.appendChild(row);
-            console.log(Memory.memoryArray[i]+".png");
+            console.log(Memory.memoryArray[i]);
            
             for (var j = 0; j < Memory.cols; j += 1)
             {
@@ -29,18 +34,11 @@ var Memory = {
                 img.src = "pics/0.png";
                 cell.appendChild(img);
                 row.appendChild(cell);
-            }
-            table.appendChild(row);
+                
+            }table.appendChild(row);
         }
         memory_board.appendChild(table);
-        
-        
-    },
-    
-    flipTile: function(){
-        
     }
-        
 };
 
 window.onload = Memory.init;
